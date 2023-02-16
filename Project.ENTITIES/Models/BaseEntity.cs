7e9c0b8ec8f3865130f4aee14751ccbac5063162
ToryanBurak <buraktoryan@gmail.com>
2023-02-16 14:15:17 +0300

    Database Tables And Relations

---
+++ Project.ENTITIES/Models/BaseEntity.cs
@@ -0,0 +1,26 @@
+﻿using Project.ENTITIES.Enu;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project.ENTITIES.Models
+{
+    public abstract class BaseEntity
+    {
+       
+            public int ID { get; set; }
+            public DateTime InstertedDate { get; set; }
+            public DateTime? ModifiedDate { get; set; }
+            public DateTime? DeletedDate { get; set; }
+            public DataStatus Status { get; set; }
+
+            public BaseEntity()
+            {
+                InstertedDate = DateTime.Now;
+                Status = DataStatus.Inserted;
+            }
+        
+    }
+}
---
+++ Project.ENTITIES/Models/Product.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project.ENTITIES.Models
+{
+    public class Product:BaseEntity
+    {
+        public string ProductName { get; set; }
+        public decimal UnitPrice { get; set; }
+
+        //Relational Properties
+
+        public virtual Category Category { get; set; }
+        public virtual List<OrderDetail> OrderDetails { get; set; }
+
+        //Special Methods
+
+        public override string ToString()
+        {
+            return $"{ProductName}..|  Price=>{UnitPrice}";
+        }
+    }
+}
